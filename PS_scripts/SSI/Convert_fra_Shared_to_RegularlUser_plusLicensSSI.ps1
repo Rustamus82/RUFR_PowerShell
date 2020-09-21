7a2e5ac355a5f040888e434fcc031b6799b229b3
Rustamus82 <rustam@live.dk>
2020-09-21 16:54:13 +0200

    encoding errors in convert scripts

--- PS_scripts/SSI/Convert_fra_Shared_to_RegularlUser_plusLicensSSI.ps1
+++ PS_scripts/SSI/Convert_fra_Shared_to_RegularlUser_plusLicensSSI.ps1
@@ -1,4 +1,4 @@
-#PSVersion 5 Script made/assembled by Rust@m 13-07-2017
+﻿#PSVersion 5 Script made/assembled by Rust@m 13-07-2017
 Write-Host "Du har valgt Convert_fra_Shared_to_RegularlUser_plusLicensSSI.ps1" -ForegroundColor Gray -BackgroundColor DarkCyan
 #*********************************************************************************************************************************************
 #Function progressbar for timeout by ctigeek:
@@ -17,7 +17,7 @@ function Start-Sleep($seconds) {
 #script execution
 #***************************************************************************
 #Variabler
-$ADuser = Read-Host -Prompt "Angiv f�llespostkasse navn som skal konverteres fra type 'shared' til type 'Regular' og Tildele Licens. - (f.eks Servicedesk)"
+$ADuser = Read-Host -Prompt "Angiv fællespostkasse navn som skal konverteres fra type 'shared' til type 'Regular' og Tildele Licens. - (f.eks Servicedesk)"
 #$ExchangeSikkerhedsgruppe = 'GRP-'+$ADuser
 #$OUPathForExchangeSikkerhedsgrupperSSI = 'OU=ResourceGroups,OU=Exchange,OU=Groups,OU=SSI,DC=SSI,DC=ad'
 #$OUPathForExchangeSikkerhedsgrupperSDS = 'OU=Exchange Sikkerhedsgrupper,OU=Sundhedsdatastyrelsen,OU=Ressourcer,DC=SSI,DC=ad'
@@ -26,11 +26,11 @@ $ADuser = Read-Host -Prompt "Angiv f�llespostkasse navn som skal konverteres f
 $OUPathSharedMailSSI_ikke_type = 'OU=Faelles postkasser ikke type shared,OU=Ressourcer,DC=SSI,DC=ad'
 #$OUPathSharedMailSDS = 'OU=Faelles postkasser,OU=Sundhedsdatastyrelsen,OU=Ressourcer,DC=SSI,DC=ad'
 $OUPathSharedMailSDS_ikke_type  = 'OU=Faelles postkasser ikke type shared,OU=Sundhedsdatastyrelsen,OU=Ressourcer,DC=SSI,DC=ad'
-$ADuserDescription = "F�llesbruger, direkte logine er mulig, skal have licens for Office 365"
+$ADuserDescription = "Fællesbruger, direkte logine er mulig, skal have licens for Office 365"
 
 
 if (-not ($ADuser -eq "*")) {
-        Write-Host "Konverterer $ADuser f�llespostkasse af type 'shared'  til 'regular' Normal User..." -foregroundcolor Cyan
+        Write-Host "Konverterer $ADuser fællespostkasse af type 'shared'  til 'regular' Normal User..." -foregroundcolor Cyan
         set-Mailbox $ADuser -Type Regular
 		
         Start-Sleep 6
@@ -43,37 +43,37 @@ if (-not ($ADuser -eq "*")) {
 
         Set-Location -Path 'SSIAD:'
         if ((Get-ADUser $ADuser -Properties "Company").company -eq "Statens Serum Institut"){
-                Write-Host "Fors�ger at flytte objekt $ADuser korrekte OU " -foregroundcolor Cyan
+                Write-Host "Forsøger at flytte objekt $ADuser korrekte OU " -foregroundcolor Cyan
                 Get-ADUser $ADuser | Move-ADObject -TargetPath "$OUPathSharedMailSSI_ikke_type"
                 Get-ADUser $ADuser |Set-ADUser -Description "$ADuserDescription"
         }
         Elseif ((Get-ADUser $ADuser -Properties "Company").company -eq "Sundhedsdatastyrelsen") {
-                Write-Host "Fors�ger at flytte objekt $ADuser korrekte OU " -foregroundcolor Cyan
+                Write-Host "Forsøger at flytte objekt $ADuser korrekte OU " -foregroundcolor Cyan
                 Get-ADUser $ADuser | Move-ADObject -TargetPath "$OUPathSharedMailSDS_ikke_type"                
         }
         Else 
-        { Write-Warning "'Copmany' felt er ikke defineret! Flyt manuelt objekt: $ADuser og dens tilh�rende $ExchangeSikkerhedsgruppe gruppe til korrekte OU i SSI AD og udfyld korrekte 'Company' felt!!"} 
+        { Write-Warning "'Copmany' felt er ikke defineret! Flyt manuelt objekt: $ADuser og dens tilhørende $ExchangeSikkerhedsgruppe gruppe til korrekte OU i SSI AD og udfyld korrekte 'Company' felt!!"} 
 
 
 
 }
-Else {write-host "Du har tastet * i username, tjek om det er korrekt f�llespostkasse, pr�v igen" -foregroundcolor Red}
+Else {write-host "Du har tastet * i username, tjek om det er korrekt fællespostkasse, prøv igen" -foregroundcolor Red}
 
 Write-Host "Connecting to Sessions" -ForegroundColor Magenta
 $reconnect =  $PSScriptRoot | Split-Path -Parent | Split-Path -Parent; Invoke-Expression "$reconnect\Logins\Session_reconnect.ps1"
 
-Write-Host "Opdaterer reggel at email som er sendt fra shared postkasse, at 'sendt post' bliver i selve f�llespostkassen." -foregroundcolor Cyan
+Write-Host "Opdaterer reggel at email som er sendt fra shared postkasse, at 'sendt post' bliver i selve fællespostkassen." -foregroundcolor Cyan
 Set-Mailbox $ADuser -MessageCopyForSentAsEnabled $false
-Write-Host "S�tter standard sprog til DK" -foregroundcolor Cyan
-Set-MailboxRegionalConfiguration �identity $ADuser �language da-dk -LocalizeDefaultFolderName
+Write-Host "Sætter standard sprog til DK" -foregroundcolor Cyan 
+Set-MailboxRegionalConfiguration –identity $ADuser –language da-dk -LocalizeDefaultFolderName
 
 Write-Host "Connecting to Sessions" -ForegroundColor Magenta
 $reconnect =  $PSScriptRoot | Split-Path -Parent | Split-Path -Parent; Invoke-Expression "$reconnect\Logins\Session_reconnect.ps1"
 
-Write-Host "�ndre kalender rettighed af $ADuser til LimitedDetails " -foregroundcolor Cyan 
+Write-Host "Ændre kalender rettighed af $ADuser til LimitedDetails " -foregroundcolor Cyan 
     $MailCalenderPath = "$ADuser" + ":\Kalender"
-    Set-mailboxfolderpermission �identity $MailCalenderPath �user Default �Accessrights LimitedDetails
-    Add-MailboxFolderPermission �Identity $MailCalenderPath �User ConciergeMobile �AccessRights Editor
+    Set-mailboxfolderpermission -identity $MailCalenderPath -user Default -Accessrights LimitedDetails
+    Add-MailboxFolderPermission -Identity $MailCalenderPath -User ConciergeMobile -AccessRights Editor
     Get-MailboxFolderPermission -Identity $MailCalenderPath
 
 Pause
--- PS_scripts/SSI/Convert_fra_Shared_to_SecureRegularUser_PlusLicens_SSI.ps1
+++ PS_scripts/SSI/Convert_fra_Shared_to_SecureRegularUser_PlusLicens_SSI.ps1
@@ -1,4 +1,4 @@
-#PSVersion 5 Script made/assembled by Rust@m 13-07-2017
+﻿#PSVersion 5 Script made/assembled by Rust@m 13-07-2017
 Write-Host "Du har valgt Convert_fra_Shared_to_SecureRegularUser_PlusLicens_SSI.ps1" -ForegroundColor Gray -BackgroundColor DarkCyan
 #*********************************************************************************************************************************************
 #Function progressbar for timeout by ctigeek:
@@ -18,7 +18,7 @@ function Start-Sleep($seconds) {
 #script execution
 #***************************************************************************
 #Variabler
-$SharedMail = Read-Host -Prompt 'Angiv f�llespostkasse navn som skal konverteres til type RegularUser og placeres for sikkermail OU - (f.eks Servicedesk):'
+$SharedMail = Read-Host -Prompt 'Angiv fællespostkasse navn som skal konverteres til type RegularUser og placeres for sikkermail OU - (f.eks Servicedesk):'
 #$OUPathForExchangeSikkerhedsgrupperSSI = 'OU=ResourceGroups,OU=Exchange,OU=Groups,OU=SSI,DC=SSI,DC=ad'
 #$OUPathSharedMailSSI = 'OU=Faelles postkasser,OU=Ressourcer,DC=SSI,DC=ad'
 #$OUPathSharedMailSSI_ikke_type = 'OU=Faelles postkasser ikke type shared,OU=Ressourcer,DC=SSI,DC=ad'
@@ -27,9 +27,9 @@ $OUPathSharedMailSSI_Sikkermail = 'OU=Sikkermail postkasser,OU=Ressourcer,DC=ssi
 #$OUPathSharedMailSDS = 'OU=Faelles postkasser,OU=Sundhedsdatastyrelsen,OU=Ressourcer,DC=SSI,DC=ad'
 #$OUPathSharedMailSDS_ikke_type  = 'OU=Faelles postkasser ikke type shared,OU=Sundhedsdatastyrelsen,OU=Ressourcer,DC=SSI,DC=ad'
 $OUPathSharedMailSDS_Sikkermail = 'OU=Sikkermail Postkasser,OU=Sundhedsdatastyrelsen,OU=Ressourcer,DC=SSI,DC=ad'
-$SharedmailDescription = "Sikkermail  (skal have licens for sikkermail l�sning fungere, direkte login muligt)"
+$SharedmailDescription = "Sikkermail  (skal have licens for sikkermail løsning fungere, direkte login muligt)"
 
-Write-Host "Konverter F�llespostkasse '$SharedMail' til type Ikke Shared"
+Write-Host "Konverter Fællespostkasse '$SharedMail' til type Ikke Shared"
 set-Mailbox $SharedMail -Type Regular
 
 Write-Host "Tildeler licens til kontoen"
@@ -42,13 +42,13 @@ if (-not ($SharedMail -eq "*")) {
 
         Set-Location -Path 'SSIAD:'
         if ((Get-ADUser $SharedMail -Properties "Company").company -eq "Statens Serum Institut"){
-                Write-Host "Fors�ger at flytte objekt $SharedMail korrekte OU " -foregroundcolor Cyan
+                Write-Host "Forsøger at flytte objekt $SharedMail korrekte OU " -foregroundcolor Cyan
                 Set-Location -Path 'SSIAD:'
                 Get-ADUser $SharedMail | Move-ADObject -TargetPath "$OUPathSharedMailSSI_Sikkermail"
                 Get-ADUser $SharedMail |Set-ADUser -Description "$SharedmailDescription"
         }
         Elseif ((Get-ADUser $SharedMail -Properties "Company").company -eq "Sundhedsdatastyrelsen") {
-                Write-Host "Fors�ger at flytte objekt $SharedMail korrekte OU " -foregroundcolor Cyan
+                Write-Host "Forsøger at flytte objekt $SharedMail korrekte OU " -foregroundcolor Cyan
                 Get-ADUser $SharedMail | Move-ADObject -TargetPath "$OUPathSharedMailSDS_Sikkermail"
                 Get-ADUser $SharedMail |Set-ADUser -Description "$SharedmailDescription"
         }
@@ -56,23 +56,23 @@ if (-not ($SharedMail -eq "*")) {
         { Write-Warning "'Copmany' felt er ikke defineret! Flyt manuelt objekt: $SharedMail  til korrekte OU i SSI AD og udfyld korrekte 'Company' felt!!"} 
 
 }
-Else { write-host "du har tastet * i username, tjek om det er korrekt f�llespostkasse/bruger" }
+Else { write-host "du har tastet * i username, tjek om det er korrekt fællespostkasse/bruger" }
 
 Write-Host "Connecting to Sessions" -ForegroundColor Magenta
 $reconnect =  $PSScriptRoot | Split-Path -Parent | Split-Path -Parent; Invoke-Expression "$reconnect\Logins\Session_reconnect.ps1"
 
-Write-Host "Opdaterer reggel at email som er sendt fra shared postkasse, at 'sendt post' bliver i selve f�llespostkassen." -foregroundcolor Cyan
+Write-Host "Opdaterer reggel at email som er sendt fra shared postkasse, at 'sendt post' bliver i selve fællespostkassen." -foregroundcolor Cyan
 Set-Mailbox $SharedMail -MessageCopyForSentAsEnabled $false
-Write-Host "S�tter standard sprog til DK" -foregroundcolor Cyan
-Set-MailboxRegionalConfiguration �identity $SharedMail �language da-dk -LocalizeDefaultFolderName
+Write-Host "Sætter standard sprog til DK" -foregroundcolor Cyan
+Set-MailboxRegionalConfiguration -identity $SharedMail -language da-dk -LocalizeDefaultFolderName
 
 Write-Host "Connecting to Sessions" -ForegroundColor Magenta
 $reconnect =  $PSScriptRoot | Split-Path -Parent | Split-Path -Parent; Invoke-Expression "$reconnect\Logins\Session_reconnect.ps1"
 
-Write-Host "�ndre kalender rettighed af $ADuser til LimitedDetails " -foregroundcolor Cyan 
+Write-Host "Ændrer kalender rettighed af $ADuser til LimitedDetails " -foregroundcolor Cyan 
     $MailCalenderPath = "$ADuser" + ":\Kalender"
-    Set-mailboxfolderpermission �identity $MailCalenderPath �user Default �Accessrights LimitedDetails
-    Add-MailboxFolderPermission �Identity $MailCalenderPath �User ConciergeMobile �AccessRights Editor
+    Set-mailboxfolderpermission -identity $MailCalenderPath -User Default -Accessrights LimitedDetails
+    Add-MailboxFolderPermission -Identity $MailCalenderPath -User ConciergeMobile -AccessRights Editor
     Get-MailboxFolderPermission -Identity $MailCalenderPath
 
 
