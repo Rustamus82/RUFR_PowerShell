29922541ef844974df45938a32694556796054ad
Rustamus82 <rustam@live.dk>
2021-03-17 12:24:40 +0100

    kode forbedret add-adgroup mass add

--- ISE_scripts_only/GroupMemberships_add_AD/add-adgroupMember_MassAdd.ps1
+++ ISE_scripts_only/GroupMemberships_add_AD/add-adgroupMember_MassAdd.ps1
@@ -1,7 +1,12 @@
-﻿#PSVersion 5 Script made/assembled by Rust@m 25-04-2019
-<#Login RUFR all AD login, Hybrid and o365
-$CommandPath = (Get-Location).Path | Split-Path -Parent; $script = "$CommandPath\Logins\Login_SSI_SST_exch2010_DKSUND_Exchange365_RUFR.ps1"; & $script
+﻿#PSVersion 5 Script made/assembled by Rust@m 17-03-2021
+#Login RUFR all AD login, Hybrid and o365
+$CommandPath = (Get-Location).Path 
+$LoginscriptISE = (Get-Location).Path | Split-Path -Parent -ErrorAction SilentlyContinue|Split-Path -Parent -ErrorAction SilentlyContinue; $LoginscriptISE = "$LoginscriptISE\Logins\Login_SSI_SST_exch2010_DKSUND_Exchange365_RUFR.ps1"; if(Test-Path $LoginscriptISE){ Invoke-Expression $LoginscriptISE}
+$ISEScriptPath = (Get-Location).Path | Split-Path -Parent -ErrorAction SilentlyContinue|Split-Path -Parent -ErrorAction SilentlyContinue; $ISEScriptPath = "$ISEScriptPath\Logins\Session_reconnect.ps1"
+$PSscriptPath =  $PSScriptRoot | Split-Path -Parent -ErrorAction SilentlyContinue | Split-Path -Parent -ErrorAction SilentlyContinue; $PSscriptPath = "$PSscriptPath\Logins\Session_reconnect.ps1"
 
+Write-Host "Connecting to Sessions" -ForegroundColor Magenta
+if (Test-Path $ISEScriptPath){ Invoke-Expression $ISEScriptPath }elseif(test-path $PSscriptPath){Invoke-Expression $PSscriptPath}
 cls
 #>
 
@@ -10,17 +15,23 @@ cls
 Set-Location -Path 'SSIAD:' 
 Set-Location -Path 'SSTAD:'
 Set-Location -Path 'DKSUNDAD:'
+Set-Location $CommandPath
 #>
 
+Set-Location $CommandPath
+$ADusers = (Get-Location).Path | Split-Path -Parent | Join-Path  -ChildPath "CSV\Users.txt"
+$ADusers = Get-Content $ADusers ;$ADusers.Count 
+$Adgroup1 = "TeamViewer_g_Users"
+$AdgroupMembers1 = Get-ADGroupMember -Identity $Adgroup1; $AdgroupMembers1.Count
+
+
 Set-Location -Path 'DKSUNDAD:'
-$ADusers = Get-Content .\CSV\Users.txt;$ADusers.Count 
 
 Foreach ($aduser in $ADusers) 
 {   
      
      Write-host -Object $("Udfører handling på {0}" -f  $ADuser) -ForegroundColor Cyan
-     Add-ADGroupMember -Identity G-ORG-SMSP-ActiveUsers $ADuser
-     Add-ADGroupMember -Identity CTX_G_DKS_RDP-Client_SST $ADuser
+     Add-ADGroupMember -Identity $Adgroup1 $ADuser -Verbose
      Start-Sleep -milliseconds 30
      
 }
